93a06a2bd5eab6d586408940675fc8d62ae47af8
Jorge Walter <jorgewalter49@gmail.com>
2023-05-15 22:17:53 -0300

    segunda pre-entrega js

--- clase.js
+++
@@ -1,9 +0,0 @@
-
-function carritoCompra(){
-    let total = 0;
-for (let i = 0; i < carrito.length; i++) {
-    total += carrito[i].precio;
-}
-console.log(`El total de la compra de ${nombre} es: $${total}`);
-}
-
